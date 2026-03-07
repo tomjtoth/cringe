31c0f952ba684b443b67983346750a3e4db501ec
Tamás Tóth <tomjtoth@gmail.com>
2026-03-07 13:44:02 +0200

    navbar as is

--- src/navbar.rs
+++ src/navbar.rs
@@ -1,9 +1,11 @@
 use dioxus::prelude::*;
 
-use crate::router::Route;
+use crate::{router::Route, state::client::use_bot_loader};
 
 #[component]
 pub fn Navbar() -> Element {
+    use_bot_loader();
+
     rsx! {
         div { class: "grow overflow-scroll", Outlet::<Route> {} }
 
@@ -13,6 +15,30 @@ pub fn Navbar() -> Element {
                 Link { to: Route::About {},
                     "⁉️"
                     span { "about" }
+                }
+            }
+            li {
+                Link { to: Route::ListOfDislikedProfiles {},
+                    "💔"
+                    span { "disliked" }
+                }
+            }
+            li {
+                Link { to: Route::ListOfUncheckedProfiles {},
+                    "🎉"
+                    span { "swipe" }
+                }
+            }
+            li {
+                Link { to: Route::ListOfLikedProfiles {},
+                    "❤️"
+                    span { "liked" }
+                }
+            }
+            li {
+                Link { to: Route::About {},
+                    "🧑"
+                    span { "profile" }
                 }
             }
         }
