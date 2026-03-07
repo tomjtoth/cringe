9c313aad8c711c30edc545af42187d7ea9fe282b
Tamás Tóth <tomjtoth@gmail.com>
2026-03-07 13:41:11 +0200

    added people listing view

---
+++ src/views/people/listing.rs
@@ -0,0 +1,57 @@
+use dioxus::prelude::*;
+
+use crate::{
+    models::person::Liked,
+    state::client::{use_bot_loader, PEEPS},
+    views::people::person::Person,
+};
+
+#[derive(Clone)]
+struct PersonFilter(fn(&crate::models::person::Person) -> bool);
+
+impl PartialEq for PersonFilter {
+    fn eq(&self, _other: &Self) -> bool {
+        true
+    }
+}
+
+#[component]
+pub fn ListOfLikedProfiles() -> Element {
+    let filter = PersonFilter(|x| matches!(x.liked, Some(Liked::Yes)));
+
+    rsx! {
+        Listing { filter }
+    }
+}
+
+#[component]
+pub fn ListOfDislikedProfiles() -> Element {
+    let filter = PersonFilter(|x| matches!(x.liked, Some(Liked::No)));
+
+    rsx! {
+        Listing { filter }
+    }
+}
+
+#[component]
+pub fn ListOfUncheckedProfiles() -> Element {
+    let filter = PersonFilter(|x| matches!(x.liked, None));
+
+    rsx! {
+        Listing { filter }
+    }
+}
+
+#[component]
+fn Listing(filter: PersonFilter) -> Element {
+    use_bot_loader();
+    rsx! {
+        ul { class: "h-full overflow-scroll",
+            for person in PEEPS.iter().filter(|person| (filter.0)(person)) {
+                li { key: "{person.id}",
+                    Person { id: &person.id }
+                }
+            }
+        }
+    }
+}
---
+++ src/views/people/mod.rs
@@ -0,0 +1,2 @@
+pub mod listing;
+mod person;
