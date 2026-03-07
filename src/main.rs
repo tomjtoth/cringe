c51b947bb6fc3f5b0558349ab1fbd93f96a14c4a
Tamás Tóth <tomjtoth@gmail.com>
2026-03-07 13:42:07 +0200

    initial step for fullstack split in fn main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use dioxus::prelude::*;
+
 mod models;
 mod navbar;
 mod router;
+mod state;
 mod views;
 
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
 fn main() {
+    #[cfg(not(feature = "server"))]
     dioxus::launch(App);
+
+    #[cfg(feature = "server")]
+    dioxus::serve(|| async { Ok(dioxus::server::router(App)) })
 }
+
 #[component]
 fn App() -> Element {
-
     rsx! {
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
