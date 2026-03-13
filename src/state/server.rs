db5a47e82b094da8a5482dcc6ca80caecf89ef20
Tamás Tóth <tomjtoth@gmail.com>
2026-03-13 08:53:07 +0200

    added test to catch broken urls in bots' images

--- src/state/server.rs
+++ src/state/server.rs
@@ -21,3 +21,43 @@ pub static BOTS: LazyLock<Vec<Person>> = LazyLock::new(|| {
         }
     }
 });
+
+#[test]
+fn no_broken_bot_img_urls() {
+    use crate::models::person::Pic;
+    use std::time::Duration;
+
+    let client = reqwest::blocking::Client::builder()
+        .user_agent("Mozilla/5.0 (compatible; cringe-bot/1.0)")
+        .timeout(Duration::from_secs(15))
+        .build()
+        .unwrap();
+
+    let pps = load_people_from_yaml().unwrap();
+
+    for pp in pps {
+        for (idx, img) in pp.pictures.iter().enumerate() {
+            let src = match img {
+                Pic::Url(src) => src,
+                Pic::Advanced { url, .. } => url,
+            };
+
+            // check only 3rd party links
+            if src.starts_with("https://") {
+                let response = client.get(src).send().unwrap_or_else(|err| {
+                    panic!(
+                        "\n\t{}'s image #{idx} request failed: '{src}'\n\tError: {err}\n",
+                        pp.name
+                    )
+                });
+                assert!(
+                    response.status().is_success(),
+                    "\n\t{}'s image #{idx} is not success: '{}'\n\tStatus: {}\n",
+                    pp.name,
+                    src,
+                    response.status()
+                );
+            }
+        }
+    }
+}
