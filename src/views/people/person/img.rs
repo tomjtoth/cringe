2bbe9ba0b2d2e997a5b42c6a30bc1a5f1c4bfd22
Tamás Tóth <tomjtoth@gmail.com>
2026-03-14 16:35:05 +0200

    person/img.rs improvements

--- src/views/people/person/img.rs
+++ src/views/people/person/img.rs
@@ -2,19 +2,25 @@ use dioxus::prelude::*;
 
 use crate::{models::person::Pic, views::people::person::container::Container};
 
+const SVG: Asset = asset!("/assets/quote.svg");
+
 #[component]
-pub fn Image(src: Option<Pic>) -> Element {
+pub fn Image(src: Option<Pic>, id: String) -> Element {
     rsx! {
         if let Some(pic) = src {
-            Container {
-                img {
-                    class: "object-cover max-md:w-full md:h-full",
-                    src: match pic {
-                        Pic::Url(src) => src.clone(),
-                        Pic::Advanced { url, .. } => url.clone(),
-                    },
-                    alt: "broken link to picture",
+            Container { id,
 
+                if let Pic::Advanced { prompt: Some(prompt), .. } = &pic {
+                    img { class: "p-2 max-w-8 inline-block", src: SVG }
+                    span { class: "p-2 text-2xl", "{prompt}" }
+                }
+
+                img {
+                    class: "object-cover w-full",
+                    src: match pic {
+                        Pic::Url(src) => src,
+                        Pic::Advanced { url, .. } => url,
+                    },
                 }
             }
         }
