ceaf09747b9260caa5605fe78ba85b674bcc1cac
Tamás Tóth <tomjtoth@gmail.com>
2026-03-14 16:36:33 +0200

    Like and Dislike button imrpos

--- src/views/people/person/buttons/like.rs
+++ src/views/people/person/buttons/like.rs
@@ -3,11 +3,16 @@ use dioxus::prelude::*;
 use crate::{models::person::Liked, state::client::PEEPS};
 
 #[component]
-pub fn LikeButton(id: String, person_liked: Option<Liked>) -> Element {
+pub fn LikeButton(id: String) -> Element {
+    let person_liked = PEEPS.iter().find(|p| p.id == id).unwrap().liked;
+
     rsx! {
         if !matches!(person_liked, Some(Liked::Yes)) {
             button {
-                class: "z-2 bottom-5 right-5",
+                class: "absolute z-2 bottom-5 right-5 p-2
+                        bg-background border rounded-full
+                        cursor-pointer select-none",
+
                 onclick: move |_| {
                     for p in PEEPS.write().iter_mut() {
                         if p.id == id {
