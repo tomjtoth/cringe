559b9e8f14176c328ac2cac8ef866c6739b4b29e
Tamás Tóth <tomjtoth@gmail.com>
2026-03-15 16:35:05 +0200

    container to accept optional additional classes

--- src/views/people/person/container.rs
+++ src/views/people/person/container.rs
@@ -3,16 +3,19 @@ use dioxus::prelude::*;
 use crate::views::people::person::buttons::LikeButton;
 
 #[component]
-pub fn Container(children: Element, id: Option<String>) -> Element {
-    rsx! {
-        div { class: "relative border rounded-2xl w-full overflow-hidden",
+pub fn Container(children: Element, id: Option<String>, class: Option<String>) -> Element {
+    let class = format!(
+        "relative border rounded-2xl w-full overflow-hidden {}",
+        class.as_deref().unwrap_or("")
+    );
 
+    rsx! {
+        div { class,
             {children}
 
             if let Some(id) = id {
                 LikeButton { id }
             }
-
         }
     }
 }
