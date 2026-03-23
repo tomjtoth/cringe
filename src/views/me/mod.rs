531368fe78f73b7b42ff73f9bdefd15cfb47669b
Tamás Tóth <tomjtoth@gmail.com>
2026-03-23 14:23:56 +0200

    /me provides None::<ListingCtx>

--- src/views/me/mod.rs
+++ src/views/me/mod.rs
@@ -2,18 +2,24 @@ use dioxus::prelude::*;
 
 use crate::{
     state::client::ME,
-    views::{me::basic::BasicMe, people::person::Person, protector::NeedsLogin},
+    views::{
+        me::basic::BasicMe,
+        people::{listing::ListingCtx, person::Person},
+        protector::NeedsLogin,
+    },
 };
 
 mod basic;
 
 #[component]
 pub fn Me() -> Element {
+    use_context_provider(|| None::<ListingCtx>);
+
     rsx! {
         NeedsLogin {
             if let Some(Some(person)) = ME() {
                 div { class: "h-full overflow-y-scroll",
-                    Person { person, wo_buttons: true }
+                    Person { person }
                 }
             } else {
                 BasicMe {}
