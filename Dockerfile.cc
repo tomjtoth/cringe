74c4bc42d3b8044753b460cf2fd1d6dc3c2afc74
Tamás Tóth <tomjtoth@gmail.com>
2026-03-24 16:20:27 +0200

    readded the --debug-symbols=false in Dock...cc

--- Dockerfile.cc
+++ Dockerfile.cc
@@ -5,7 +5,7 @@ COPY . .
 RUN --mount=type=cache,id=cargo-registry,target=/usr/local/cargo/registry \
     --mount=type=cache,id=cargo-git,target=/usr/local/cargo/git \
     --mount=type=cache,id=cargo-target,target=/app/target \
-    dx build --release --verbose && \
+    dx build --release --verbose --debug-symbols=false && \
     mv /app/target/dx/cringe/release/web/ /app.built/ && \
     cp -a /app/migrations/ /app.built/migrations/
 
@@ -19,5 +19,5 @@ COPY --from=builder /app.built/ /app/
 ENV IP=0.0.0.0 \
     PORT=80 \
     APP_VER=$APP_VER
-    
+
 ENTRYPOINT ["/app/cringe"]
