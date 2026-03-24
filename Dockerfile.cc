289f6bc739d4a0cfd8e61645b7b73c0a4c48bc66
Tamás Tóth <tomjtoth@gmail.com>
2026-03-24 12:44:40 +0200

    added APP_VER to build-args

--- Dockerfile.cc
+++ Dockerfile.cc
@@ -1,6 +1,8 @@
 FROM lewimbes/dioxus AS builder
 WORKDIR /app
 
+ARG APP_VER=prod
+
 COPY . .
 RUN dx build --release --verbose && \
     mv /app/target/dx/cringe/release/web/ /app.built/ && \
@@ -12,5 +14,6 @@ WORKDIR /app
 COPY --from=builder /app.built/ /app/
 
 ENV IP=0.0.0.0 \
-    PORT=80
+    PORT=80 \
+    APP_VER=$APP_VER
 ENTRYPOINT ["/app/cringe"]
