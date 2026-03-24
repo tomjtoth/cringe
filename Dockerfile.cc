006c9236196b73dacb8eb0a266b88bb0fc635ad3
Tamás Tóth <tomjtoth@gmail.com>
2026-03-24 08:55:05 +0200

    rm explicit caching flags for Dockerfile's RUN directive & try gnu target to save time

---
+++ Dockerfile.cc
@@ -0,0 +1,16 @@
+FROM lewimbes/dioxus AS builder
+WORKDIR /app
+
+COPY . .
+RUN dx build --release --verbose && \
+    mv /app/target/dx/cringe/release/web /app.built && \
+    cp -a /app/migrations/ /app.built/migrations/
+
+FROM gcr.io/distroless/cc
+WORKDIR /app
+
+COPY --from=builder /app.built/ /app/
+
+ENV IP=0.0.0.0 \
+    PORT=80
+ENTRYPOINT ["/app/cringe"]
