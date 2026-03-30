a6e1620bd20162732d88f3c446a36a0235abd8ed
Tamás Tóth <tomjtoth@gmail.com>
2026-03-30 20:17:26 +0300

    rm LogoutButton from the Me View

--- src/views/me/mod.rs
+++ src/views/me/mod.rs
@@ -20,15 +20,6 @@ pub fn Me() -> Element {
             if let Some(Some(person)) = ME() {
                 div { class: "relative h-full overflow-y-scroll",
                     Person { person }
-                    a {
-                        class: format!(
-                            "{} {}",
-                            "absolute left-1/2 -translate-x-1/2 bottom-2",
-                            "border rounded p-2 cursor-pointer select-none",
-                        ),
-                        href: "/logout",
-                        "logout"
-                    }
                 }
             } else {
                 BasicMe {}
