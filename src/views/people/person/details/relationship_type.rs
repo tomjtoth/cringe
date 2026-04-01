6b83553ead6455e5e0375e4d7505843d02f238c2
Tamás Tóth <tomjtoth@gmail.com>
2026-04-01 19:10:59 +0300

    unified rendering logic in Seeking and RelationshipType

--- src/views/people/person/details/relationship_type.rs
+++ src/views/people/person/details/relationship_type.rs
@@ -4,43 +4,33 @@ use crate::models::person::{Person, RelationshipType as ERT};
 
 #[component]
 pub(super) fn RelationshipType(sig: Signal<Person>, editing: bool) -> Element {
+    let tmp = sig.read().relationship_type;
+    let r = tmp.as_ref();
+    let value = r.map(|rt| rt.to_string());
+
     rsx! {
         if editing {
-            {
-                let value = sig
-                    .read()
-                    .relationship_type
-                    .map(|s| s.to_string())
-                    .unwrap_or("".to_string());
+            div {
+                select {
+                    class: if value == None { "text-gray-500" },
+                    value,
+                    onchange: move |evt| {
+                        sig.write().relationship_type = ERT::from_str(&evt.value());
+                    },
 
-                rsx! {
-                    div {
-                        select {
-                            class: if value == "" { "text-gray-500" },
-                            value,
-                            onchange: move |evt| {
-                                sig.write().relationship_type = ERT::from_str(&evt.value());
-                            },
-
-                            option { value: "", "👩‍❤️‍👨 Your relationship type..." }
-                            option { value: "{ERT::FiguringOutMyRelationshipType}", "{ERT::FiguringOutMyRelationshipType}" }
-                            option { value: "{ERT::Monogamy}", "{ERT::Monogamy}" }
-                            option { value: "{ERT::NonMonogamy}", "{ERT::NonMonogamy}" }
-                        }
+                    option { value: "", "👩‍❤️‍👨 Your relationship type..." }
+                    option { value: "{ERT::FiguringOutMyRelationshipType}",
+                        "{ERT::FiguringOutMyRelationshipType}"
                     }
+                    option { value: "{ERT::Monogamy}", "{ERT::Monogamy}" }
+                    option { value: "{ERT::NonMonogamy}", "{ERT::NonMonogamy}" }
                 }
             }
         } else {
-            if let Some(relationship_type) = &sig.read().relationship_type {
-                {
-                    let (emoji, text) = relationship_type.parts();
-
-                    rsx! {
-                        div {
-                            "{emoji}"
-                            div { "{text}" }
-                        }
-                    }
+            if let Some((emoji, text)) = r.map(|rt| rt.parts()) {
+                div {
+                    "{emoji}"
+                    div { "{text}" }
                 }
             }
         }
--- src/views/people/person/details/seeking.rs
+++ src/views/people/person/details/seeking.rs
@@ -4,48 +4,33 @@ use crate::models::person::{Person, Seeking as ES};
 
 #[component]
 pub(super) fn Seeking(sig: Signal<Person>, editing: bool) -> Element {
+    let tmp = sig.read().seeking;
+    let r = tmp.as_ref();
+    let value = r.map(|s| s.to_string());
+
     rsx! {
         if editing {
-            {
-                let value = sig
-                    .read()
-                    .seeking
-                    .as_ref()
-                    .map(|s| s.to_string())
-                    .unwrap_or("".to_string());
+            div {
+                select {
+                    class: if value == None { "text-gray-500" },
+                    value,
+                    onchange: move |evt| {
+                        sig.write().seeking = ES::from_str(&evt.value());
+                    },
 
-                rsx! {
-                    div {
-
-                        select {
-                            class: if value == "" { "text-gray-500" },
-
-                            value,
-                            onchange: move |evt| {
-                                sig.write().seeking = ES::from_str(&evt.value());
-                            },
-
-                            option { value: "", class: "text-gray-500", "🕵️ You're seeking..." }
-                            option { value: "{ES::LongTerm}", "{ES::LongTerm}" }
-                            option { value: "{ES::LongTermOpenToShort}", "{ES::LongTermOpenToShort}" }
-                            option { value: "{ES::ShortTermFun}", "{ES::ShortTermFun}" }
-                            option { value: "{ES::ShortTermOpenToLong}", "{ES::ShortTermOpenToLong}" }
-                            option { value: "{ES::StillFiguringItOut}", "{ES::StillFiguringItOut}" }
-                        }
-                    }
+                    option { value: "", class: "text-gray-500", "🕵️ You're seeking..." }
+                    option { value: "{ES::LongTerm}", "{ES::LongTerm}" }
+                    option { value: "{ES::LongTermOpenToShort}", "{ES::LongTermOpenToShort}" }
+                    option { value: "{ES::ShortTermFun}", "{ES::ShortTermFun}" }
+                    option { value: "{ES::ShortTermOpenToLong}", "{ES::ShortTermOpenToLong}" }
+                    option { value: "{ES::StillFiguringItOut}", "{ES::StillFiguringItOut}" }
                 }
             }
         } else {
-            if let Some(seeking) = &sig.read().seeking {
-                {
-                    let (emoji, text) = seeking.parts();
-
-                    rsx! {
-                        div {
-                            "{emoji}"
-                            div { "{text}" }
-                        }
-                    }
+            if let Some((emoji, text)) = r.map(|s| s.parts()) {
+                div {
+                    "{emoji}"
+                    div { "{text}" }
                 }
             }
         }
