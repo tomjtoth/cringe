307de64cccd03474207d27d3a3e0094378e8b4ee
Tamás Tóth <tomjtoth@gmail.com>
2026-04-03 23:37:10 +0300

    relationship_type refactored

--- src/models/person.rs
+++ src/models/person.rs
@@ -8,6 +8,7 @@ use crate::models::{
     gender::Gender,
     image::{Image, TImages},
     kids::TKids,
+    relationship_type::RelationshipType,
     seeking::Seeking,
 };
 
@@ -74,51 +75,6 @@ impl Frequency {
 impl std::fmt::Display for Frequency {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(self.label())
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
-#[cfg_attr(feature = "server", derive(sqlx::Type))]
-#[cfg_attr(feature = "server", sqlx(type_name = "relationship_type"))]
-pub enum RelationshipType {
-    #[serde(rename = "monogamy")]
-    #[cfg_attr(feature = "server", sqlx(rename = "monogamy"))]
-    Monogamy,
-    #[serde(rename = "non-monogamy")]
-    #[cfg_attr(feature = "server", sqlx(rename = "non-monogamy"))]
-    NonMonogamy,
-    #[serde(rename = "figuring out my relationship type")]
-    #[cfg_attr(feature = "server", sqlx(rename = "figuring out my relationship type"))]
-    FiguringOutMyRelationshipType,
-}
-
-impl RelationshipType {
-    pub fn parts(&self) -> (&str, &str) {
-        match self {
-            Self::Monogamy => ("💍", "Monogamy"),
-            Self::NonMonogamy => ("🌈", "Non-monogamy"),
-            Self::FiguringOutMyRelationshipType => ("🧭", "Figuring out my relationship type"),
-        }
-    }
-
-    pub fn from_str(s: &str) -> Option<Self> {
-        let parts = s.split_once(" ")?;
-
-        [
-            Self::Monogamy,
-            Self::NonMonogamy,
-            Self::FiguringOutMyRelationshipType,
-        ]
-        .into_iter()
-        .find(|g| g.parts() == parts)
-    }
-}
-
-impl std::fmt::Display for RelationshipType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let (emoji, text) = self.parts();
-        let label = &format!("{emoji} {text}");
-        f.write_str(label)
     }
 }
 
