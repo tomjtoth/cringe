df3366a07d5335cd9690ee86ce71dae7324b1ec5
Tamás Tóth <tomjtoth@gmail.com>
2026-04-04 07:32:51 +0300

    changed Container into a form

--- src/views/people/person/container.rs
+++ src/views/people/person/container.rs
@@ -3,14 +3,28 @@ use dioxus::prelude::*;
 use crate::views::people::person::button::LikeButton;
 
 #[component]
-pub fn Container(children: Element, class: Option<String>, wo_button: Option<bool>) -> Element {
+pub fn Container(
+    children: Element,
+    class: Option<String>,
+    wo_button: Option<bool>,
+    onsubmit: Option<Callback<Event<FormData>>>,
+) -> Element {
     let class = format!(
         "relative border rounded-2xl w-full overflow-hidden {}",
         class.as_deref().unwrap_or("")
     );
 
     rsx! {
-        div { class,
+        form {
+            class,
+            onsubmit: move |evt| {
+                evt.prevent_default();
+
+                if let Some(handler) = onsubmit {
+                    handler(evt);
+                }
+            },
+
             {children}
 
             if wo_button != Some(true) {
