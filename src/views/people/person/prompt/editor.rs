2af855bc495c8a77172c7a17f6571339ea5fd8a3
Tamás Tóth <tomjtoth@gmail.com>
2026-04-06 12:34:19 +0300

    more similar PromptEditor behavior to ImageEditor

--- src/views/people/person/prompt/editor.rs
+++ src/views/people/person/prompt/editor.rs
@@ -3,34 +3,32 @@ use serde::{Deserialize, Serialize};
 
 use crate::models::person::Prompt;
 use crate::state::client::{AUTH_CTE, ME};
-use crate::views::people::person::{container::Container, ResourceCtx};
+use crate::views::people::person::{container::Container, despair::Despair, ResourceCtx, Sorter};
 
 #[cfg_attr(feature = "server", derive(sqlx::Type))]
 #[cfg_attr(feature = "server", derive(sqlx::FromRow))]
 #[derive(Serialize, Deserialize, Default, Debug)]
-struct PromptUpdater {
+struct Response {
     authorized: bool,
-    updated_positions: bool,
-    deleted_prompt: bool,
-    updated_prompt: bool,
-    inserted_prompt_id: Option<i32>,
+    sorted: i64,
+    deleted: i64,
+    updated: i64,
+    inserted_id: Option<i32>,
 }
 
 #[put("/api/me/prompts")]
-async fn mod_prompt(
-    prompt: Prompt,
-    positions: Vec<(Option<i32>, Option<i16>)>,
-) -> Result<PromptUpdater> {
+async fn mod_prompt(prompt: Prompt, sorter: Sorter) -> Result<Response> {
     use sqlx::types::Json;
 
-    info!("Received {prompt:?} {positions:?}");
+    info!("Received {prompt:?} {sorter:?}");
 
-    let mut res = PromptUpdater::default();
+    let mut res = Response::default();
 
     if let (Some(sess_id), pool) = crate::state::server::get_ctx().await {
-        let (ids, positions): (Vec<Option<i32>>, Vec<Option<i16>>) = positions.into_iter().unzip();
+        let (sorter_ids, sorter_pos): (Vec<Option<i32>>, Vec<Option<i16>>) =
+            sorter.into_iter().unzip();
 
-        Json(res) = sqlx::query_scalar::<_, Json<PromptUpdater>>(&format!(
+        Json(res) = sqlx::query_scalar::<_, Json<Response>>(&format!(
             r"
             WITH {AUTH_CTE},
             
@@ -49,42 +47,42 @@ async fn mod_prompt(
                 )
             ),
 
-            arg_positions AS (
+            arg_sorter AS (
                 SELECT * FROM unnest($3::int[], $4::smallint[]) 
                 AS _x(id, position)
             ),
 
-            updated_pos AS (
+            sorted AS (
                 UPDATE user_prompts AS up
                 SET position = pos.position
-                FROM arg_positions pos
+                FROM arg_sorter pos
                 CROSS JOIN me
                 WHERE up.user_id = me.id
                 AND up.id = pos.id
                 RETURNING up.id
             ),
 
-            deleted_prompt AS (
+            deleted AS (
                 DELETE FROM user_prompts AS up
                 USING arg_prompt AS p
                 CROSS JOIN me
                 WHERE up.user_id = me.id 
-                AND up.id = p.id AND (p.title = '' OR p.body = '')
+                AND up.id = p.id AND (p.title = '' OR p.body = '' OR p.position IS NULL)
                 RETURNING up.id
             ),
 
-            inserted_prompt AS (
+            inserted AS (
                 INSERT INTO user_prompts AS up
                     (user_id, position, title, body)
                 SELECT
                     me.id, p.position, p.title, p.body
-                FROM arg_prompt p 
+                FROM arg_prompt p
                 CROSS JOIN me
                 WHERE p.id IS NULL
                 RETURNING new.id
             ),
 
-            updated_prompt AS (
+            updated AS (
                 UPDATE user_prompts AS up
                 SET 
                     position = p.position,
@@ -93,23 +91,23 @@ async fn mod_prompt(
                 FROM arg_prompt AS p
                 CROSS JOIN me
                 WHERE up.user_id = me.id AND up.id = p.id
-                AND NOT (p.title = '' OR p.body = '')
+                AND NOT (p.title = '' OR p.body = '' OR p.position IS NULL)
                 RETURNING up.id
             )
 
             SELECT jsonb_build_object(
                 'authorized', (SELECT count(*) > 0 FROM me),
-                'updated_positions', (SELECT count(*) > 0 FROM updated_pos),
-                'deleted_prompt', (SELECT count(*) > 0 FROM deleted_prompt),
-                'updated_prompt', (SELECT count(*) > 0 FROM updated_prompt),
-                'inserted_prompt_id', (SELECT id FROM inserted_prompt LIMIT 1)
+                'sorted', (SELECT count(*) FROM sorted),
+                'deleted', (SELECT count(*) FROM deleted),
+                'updated', (SELECT count(*) FROM updated),
+                'inserted_id', (SELECT id FROM inserted)
             )
             "
         ))
         .bind(&sess_id)
         .bind(Json(&prompt))
-        .bind(&ids)
-        .bind(&positions)
+        .bind(&sorter_ids)
+        .bind(&sorter_pos)
         .fetch_one(&pool)
         .await?;
     }
@@ -121,38 +119,46 @@ async fn mod_prompt(
 pub(super) fn PromptEditor(src: Option<Prompt>) -> Element {
     let rcx = use_context::<ResourceCtx>();
 
-    let mut sig = use_signal(|| src.unwrap_or(Prompt::default()));
-
     let max = ME
         .read()
         .profile
         .as_ref()
-        .map(|p| p.prompts().len())
+        .map(|p| p.prompts().len() as i16)
         .unwrap_or(0)
         + {
             // only a new prompt can be added as additional
-            if sig.read().id.is_none() {
+            if src.is_none() {
                 1
             } else {
                 0
             }
         };
 
-    // this is the submit handler I failed to attach to the Container's would-be form tag
-    use_resource({
+    let mut sig = use_signal(|| {
+        src.unwrap_or(Prompt {
+            position: Some(max - 1),
+            ..Default::default()
+        })
+    });
+
+    let new_but_empty =
+        sig.with(|p| p.id == None && (p.title == "" || p.body == "" || p.position == None));
+
+    let onsubmit = use_callback({
         let rcx = rcx.clone();
 
-        move || {
-            let mut rcx = rcx.clone();
+        move |_: Event<FormData>| {
+            spawn({
+                let mut rcx = rcx.clone();
 
-            async move {
-                if rcx.submitting() {
+                async move {
+                    rcx.next_state();
+
+                    if new_but_empty {
+                        return rcx.next_state();
+                    }
+
                     info!("Submitting prompt: {:?}", sig.read());
-
-                    let Some(prompt_pos) = sig.read().position else {
-                        // position must be defined
-                        return;
-                    };
 
                     // getting a detached clone of the whole array to accommodate changes
                     let mut draft = ME
@@ -162,15 +168,17 @@ pub(super) fn PromptEditor(src: Option<Prompt>) -> Element {
                         .map(|p| p.prompts().clone())
                         .unwrap_or(vec![]);
 
-                    let mut positions = vec![];
-
-                    let prompt_id = sig().id;
+                    let prompt_id = sig.read().id;
                     draft.retain(|pp| pp.id != prompt_id);
 
-                    // DELETE by not re-inserting at desired position
-                    if !(sig.read().title == "" || sig.read().body == "") {
-                        draft.insert(prompt_pos as usize, sig());
+                    // achieving DELETE by not re-inserting at desired position
+                    if let Some(pos) =
+                        sig.with(|p| p.position.filter(|_| p.title != "" && p.body != ""))
+                    {
+                        draft.insert(pos as usize, sig());
                     }
+
+                    let mut positions = vec![];
 
                     // adjust positions of each prompt
                     for (idx, prompt) in draft.iter_mut().enumerate() {
@@ -184,53 +192,69 @@ pub(super) fn PromptEditor(src: Option<Prompt>) -> Element {
                         });
                     }
 
-                    if let Ok(res) = mod_prompt(sig(), positions).await {
+                    if let Ok(Response {
+                        authorized: true,
+                        inserted_id,
+                        ..
+                    }) = mod_prompt(sig(), positions).await.map(|res| {
                         info!("PUT /api/me/prompts returned: {:?}", res);
-
-                        if let PromptUpdater {
-                            authorized: true,
-                            inserted_prompt_id,
-                            ..
-                        } = res
-                        {
-                            if inserted_prompt_id.is_some() {
-                                // paste the ID received from the server to its prompt based on pos
-                                draft.get_mut(prompt_pos as usize).map(|p| {
-                                    p.id = inserted_prompt_id;
-                                });
-                            }
-
-                            // move resource context out of the submitting phase
-                            // else I get an endless loop :)
-                            rcx.next_state();
-
-                            // sync state
-                            ME.with_mut(|p| {
-                                p.profile.as_mut().map(|p| {
-                                    p.prompts.truncate(0);
-                                    p.prompts.append(&mut draft);
-                                });
+                        res
+                    }) {
+                        // paste the ID received from the server to its prompt based on pos
+                        if let Some(pos) = sig.read().position.filter(|_| inserted_id.is_some()) {
+                            draft.get_mut(pos as usize).map(|p| {
+                                p.id = inserted_id;
                             });
-
-                            return;
                         }
+
+                        // sync state
+                        ME.with_mut(|p| {
+                            p.profile.as_mut().map(|p| {
+                                p.prompts.truncate(0);
+                                p.prompts.append(&mut draft);
+                            });
+                        });
                     }
+
                     rcx.next_state();
                 }
-            }
+            });
         }
     });
 
     let disabled = !rcx.editing();
 
+    let to_be_deleted =
+        sig.with(|p| p.id.is_some() && (p.title == "" || p.body == "" || p.position == None));
+
+    let bg_red = if to_be_deleted {
+        " bg-red-200 dark:bg-red-800"
+    } else {
+        ""
+    };
+
+    let class = format!("pt-10 pb-20 px-2 grid grid-cols-[1fr_auto] gap-2{bg_red}");
+
+    let button_class = "z-2 absolute bottom-5 right-5 border-2! bg-background select-none";
+
     rsx! {
-        Container { class: "pt-10 pb-20 px-2 grid grid-cols-[1fr-auto] gap-2",
+        Container { class, onsubmit,
+
+            if new_but_empty {
+                button { class: button_class, "Need all values! ☝️" }
+            }
+
+            if to_be_deleted {
+                Despair {}
+                button { class: "{button_class}{bg_red}", "That's how you delete! 😱" }
+            }
+
             input {
-                class: "p-2 m-2 mr-0 text-xl min-w-30",
+                class: "min-w-20 w-full",
                 placeholder: "Title",
                 disabled,
-                value: sig.read().title.clone(),
-                onchange: move |evt| sig.with_mut(|p| p.title = evt.value()),
+                value: sig.read().title.as_ref(),
+                oninput: move |evt| sig.write().title = evt.value(),
             }
 
             input {
@@ -239,23 +263,26 @@ pub(super) fn PromptEditor(src: Option<Prompt>) -> Element {
                 min: 1,
                 max,
                 r#type: "number",
-                value: sig.read().position.map(|p| p + 1),
-                onchange: move |evt| {
-                    if let Ok(pos) = evt.value().parse::<i16>() {
-                        if 0 <= pos && pos <= (max as i16) {
-                            sig.with_mut(|p| p.position = Some(pos - 1));
-                        }
-                    }
+                class: "max-w-20",
+                value: sig.read().position.map(|pos| pos + 1),
+                oninput: move |evt| {
+                    sig.write().position = evt
+                        .value()
+                        .parse::<i16>()
+                        .ok()
+                        .filter(|&pos| 1 <= pos && pos <= max)
+                        .map(|pos| pos - 1);
                 },
             }
 
             textarea {
-                class: "border rounded p-2 col-span-2 text-2xl",
+                class: "col-span-2 text-2xl",
                 placeholder: "Body",
                 disabled,
-                value: sig.read().body.clone(),
-                onchange: move |evt| sig.with_mut(|p| p.body = evt.value()),
+                value: sig.read().body.as_ref(),
+                oninput: move |evt| sig.write().body = evt.value(),
             }
+
         }
     }
 }
