2192cfecd613df99058bf92e63ddc5708535bcd1
Tamás Tóth <tomjtoth@gmail.com>
2026-04-13 19:12:39 +0300

    impl budget random u32 generator for te browser

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,19 @@ pub async fn sleep(secs: u64) {
     #[cfg(target_arch = "wasm32")]
     gloo_timers::future::sleep(duration).await;
 }
+
+pub fn rand_u32() -> u32 {
+    let mut buf = [0u8; 4];
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use web_sys::window;
+        let window = window().expect("no window");
+        let crypto = window.crypto().expect("no crypto");
+        crypto
+            .get_random_values_with_u8_array(&mut buf)
+            .expect("failed to get random");
+    }
+
+    u32::from_le_bytes(buf)
+}
