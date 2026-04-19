6711c6ccd822e86326af48b2e52c81686006f494
Tamás Tóth <tomjtoth@gmail.com>
2026-04-19 19:43:39 +0300

    call use_context once per Habits component, only borrow &dcx

--- src/views/people/profile/details/habits.rs
+++ src/views/people/profile/details/habits.rs
@@ -4,14 +4,36 @@ use crate::models::person::{Frequency, THabits};
 use crate::views::people::profile::details::DetailsCtx;
 
 #[component]
-pub(super) fn Habit(
-    children: Element,
-    emoji: String,
+pub(super) fn Habits() -> Element {
+    let dcx = use_context::<DetailsCtx>();
+
+    rsx! {
+        {habit("🍷", "Drinking", &dcx, |h| h.drinking.as_ref(), |h, f| h.drinking = f)}
+
+        {habit("🚬", "Smoking", &dcx, |h| h.smoking.as_ref(), |h, f| h.smoking = f)}
+
+        {
+            habit(
+                "🌿🚬",
+                "Marijuana",
+                &dcx,
+                |h| h.marijuana.as_ref(),
+                |h, f| h.marijuana = f,
+            )
+        }
+
+        {habit("💊💉", "Drugs", &dcx, |h| h.drugs.as_ref(), |h, f| h.drugs = f)}
+    }
+}
+
+/// keeping this a simple fn due to selector and onchange
+fn habit(
+    emoji: &str,
+    question: &str,
+    dcx: &DetailsCtx,
     selector: fn(&THabits) -> Option<&Frequency>,
     onchange: fn(&mut THabits, Option<Frequency>),
 ) -> Element {
-    let mut dcx = use_context::<DetailsCtx>();
-
     let tmp_ro = &dcx.ro.read().habits;
     let ro = tmp_ro.as_ref();
 
@@ -21,6 +43,8 @@ pub(super) fn Habit(
         .and_then(selector)
         .map(|d| d.to_string())
         .unwrap_or_default();
+
+    let mut rw = dcx.rw;
 
     rsx! {
         if (dcx.editing)() {
@@ -32,18 +56,17 @@ pub(super) fn Habit(
                     value,
 
                     onchange: move |evt| {
-                        dcx.rw
+                        rw
                             .with_mut(|p| {
-                                let freq = Frequency::from_str(&evt.value());
-                                // TODO: fallback could be implemented, but this is always populated as of 19.4
-                                if let Some(h) = p.habits.as_mut() {
-                                    onchange(h, freq);
-                                }
-                            });
+                            let freq = Frequency::from_str(&evt.value());
+                            // TODO: fallback could be implemented, but this is always populated as of 19.4
+                            if let Some(h) = p.habits.as_mut() {
+                                onchange(h, freq);
+                            }
+                        });
                     },
 
-                    {children}
-
+                    option { value: "", "{question}?" }
                     option { value: "{Frequency::Never}", "{Frequency::Never}" }
                     option { value: "{Frequency::Rarely}", "{Frequency::Rarely}" }
                     option { value: "{Frequency::Often}", "{Frequency::Often}" }
