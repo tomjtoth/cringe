b41b221279867e0553509b1d7759a4c32230ace0
Tamás Tóth <tomjtoth@gmail.com>
2026-04-19 09:58:55 +0300

    rm Crypto from web-sys dep's features along with te custom random number generator fn

--- src/utils.rs
+++ src/utils.rs
@@ -7,20 +7,3 @@ pub async fn sleep(secs: u64) {
     #[cfg(target_arch = "wasm32")]
     gloo_timers::future::sleep(duration).await;
 }
-
-pub fn random_id() -> u128 {
-    #[allow(unused_mut)]
-    let mut buf = [0u8; 16];
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        use web_sys::window;
-        let window = window().expect("no window");
-        let crypto = window.crypto().expect("no crypto");
-        crypto
-            .get_random_values_with_u8_array(&mut buf)
-            .expect("failed to get random");
-    }
-
-    u128::from_le_bytes(buf)
-}
