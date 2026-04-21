04b8b011a8c75adb761888e7c06b68a29a8d9665
Tamás Tóth <tomjtoth@gmail.com>
2026-04-21 11:25:17 +0300

    revisited Seeking

--- src/models/seeking.rs
+++ src/models/seeking.rs
@@ -1,28 +1,33 @@
 use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
 
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize, strum_macros::EnumIter)]
 #[cfg_attr(feature = "server", derive(sqlx::Type))]
 #[cfg_attr(feature = "server", sqlx(type_name = "seeking"))]
 pub enum Seeking {
     #[serde(rename = "short-term fun")]
     #[cfg_attr(feature = "server", sqlx(rename = "short-term fun"))]
     ShortTermFun,
+
     #[serde(rename = "short-term, open to long")]
     #[cfg_attr(feature = "server", sqlx(rename = "short-term, open to long"))]
     ShortTermOpenToLong,
+
     #[serde(rename = "long-term, open to short")]
     #[cfg_attr(feature = "server", sqlx(rename = "long-term, open to short"))]
     LongTermOpenToShort,
+
     #[serde(rename = "long-term")]
     #[cfg_attr(feature = "server", sqlx(rename = "long-term"))]
     LongTerm,
+
     #[serde(rename = "still figuring it out")]
     #[cfg_attr(feature = "server", sqlx(rename = "still figuring it out"))]
     StillFiguringItOut,
 }
 
 impl Seeking {
-    pub fn parts(&self) -> (&str, &str) {
+    pub const fn parts(&self) -> (&str, &str) {
         match self {
             Self::ShortTermFun => ("🎉", "Short-term fun"),
             Self::ShortTermOpenToLong => ("🪄", "Short-term, open to long"),
@@ -35,23 +40,13 @@ impl Seeking {
     pub fn from_str(s: &str) -> Option<Self> {
         let parts = s.split_once(" ")?;
 
-        [
-            Self::ShortTermFun,
-            Self::ShortTermOpenToLong,
-            Self::LongTermOpenToShort,
-            Self::LongTerm,
-            Self::StillFiguringItOut,
-        ]
-        .into_iter()
-        .find(|g| g.parts() == parts)
+        Self::iter().find(|g| g.parts() == parts)
     }
 }
 
 impl std::fmt::Display for Seeking {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let (emoji, text) = self.parts();
-        let label = &format!("{emoji} {text}",);
-
-        f.write_str(label)
+        f.write_str(&format!("{emoji} {text}"))
     }
 }
