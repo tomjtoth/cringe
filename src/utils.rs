f46acdd2e79a81c392270e4613c31ec6c0f63366
Tamás Tóth <tomjtoth@gmail.com>
2026-04-25 08:50:36 +0300

    refactored async fn sleep

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,19 @@
-pub async fn sleep(secs: u64) {
-    let duration = std::time::Duration::from_secs(secs);
+use std::time::Duration;
 
+async fn _sleep(duration: Duration) {
     #[cfg(feature = "server")]
     tokio::time::sleep(duration).await;
 
     #[cfg(target_arch = "wasm32")]
     gloo_timers::future::sleep(duration).await;
 }
+
+pub async fn sleep(secs: u64) {
+    let duration = Duration::from_secs(secs);
+    _sleep(duration).await;
+}
+
+// pub async fn sleep_ms(millis: u64) {
+//     let duration = Duration::from_millis(millis);
+//     _sleep(duration).await;
+// }
