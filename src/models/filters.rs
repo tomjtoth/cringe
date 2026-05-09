3535834dc8a920eed43ff2e99b9b5df8cf4e10b1
Tamás Tóth <tomjtoth@gmail.com>
2026-05-09 21:20:35 +0300

    vectors of filters to be initialized form NULLs in DB

--- src/models/filters.rs
+++ src/models/filters.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{de::Deserializer, Deserialize, Serialize};
 
 use crate::models::{
     FamilyPlans, Frequency, Gender, GenderIdentity, RelationshipType, Seeking, ZodiacSign,
@@ -6,28 +6,46 @@ use crate::models::{
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
 pub struct Filters {
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub gender: Vec<Gender>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub gender_identity: Vec<GenderIdentity>,
 
     pub age_min: Option<u16>,
     pub age_max: Option<u16>,
 
-    pub height_min: Option<u16>,
-    pub height_max: Option<u16>,
+    pub height_min: Option<u8>,
+    pub height_max: Option<u8>,
 
     pub distance: Option<u16>,
 
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub zodiac_sign: Vec<ZodiacSign>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub seeking: Vec<Seeking>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub relationship_type: Vec<RelationshipType>,
 
     pub has_children: Option<bool>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub family_plans: Vec<FamilyPlans>,
 
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub drinking: Vec<Frequency>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub smoking: Vec<Frequency>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub marijuana: Vec<Frequency>,
+    #[serde(deserialize_with = "null_as_empty_vec")]
     pub drugs: Vec<Frequency>,
 
-    pub image_count_min: u8,
+    pub image_count_min: Option<u8>,
+}
+
+fn null_as_empty_vec<'de, D, T>(deserializer: D) -> Result<Vec<T>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: serde::Deserialize<'de>,
+{
+    Ok(Option::<Vec<T>>::deserialize(deserializer)?.unwrap_or_default())
 }
